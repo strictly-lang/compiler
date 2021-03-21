6aadf22e70ab5d271b0ebf4875a5638697a6cd84
plusgut <github@webentwickler2-0.de>
2021-03-21 17:09:09 +0100

    [ast] add line-counting

--- src-lib/Parser/Main.hs
+++ src-lib/Parser/Main.hs
@@ -1,37 +1,39 @@
 module Parser.Main where
 
-type LineCount = Int;
-type IndentedLine = (Int, String);
-type Option = [String];
-type Position = (Int, Int);
-type NodeName = String;
-type ErrorTuple = (String, Position);
-type NodeTuple = (NodeName, LineCount, [Option], [MaybeNode])
-data MaybeNode = Node NodeTuple | Error ErrorTuple;
+type Line = Int
+type Column = Int
+type IndentationLevel = Int
+type IndentedLine = (Int, String)
+type Option = [String]
+type Range = (Line, Column, Column)
+type NodeName = String
+type ErrorTuple = (String, Range)
+type NodeTuple = (NodeName, Line, [Option], [MaybeNode])
+data MaybeNode = Node NodeTuple | Error ErrorTuple
 
 parse content = parseRoot( map getIndentation (lines content))
 
 getIndentation :: String -> IndentedLine
 getIndentation ('\t':xs) =
-    let (indent, value) = getIndentation xs;
+    let (indent, value) = getIndentation xs
     in (indent + 1 , value)
-getIndentation xs = (0, xs);
+getIndentation xs = (0, xs)
 
-parseRoot :: [IndentedLine] -> [MaybeNode];
-parseRoot indentedLines = [Node ("foo", 1, [], [])]
+parseRoot :: [IndentedLine] -> [MaybeNode]
+parseRoot indentedLines =
+    let (rootNodes, restLines) = parseIndent 0 0 indentedLines
+    in rootNodes
 
-
-parseIndent :: LineCount -> [IndentedLine] -> ([MaybeNode], [IndentedLine])
-parseIndent _ [] = ([], [])
-parseIndent indentLevel indentedLines
+parseIndent :: IndentationLevel -> Line -> [IndentedLine] -> ([MaybeNode], [IndentedLine])
+parseIndent _ _ [] = ([], [])
+parseIndent indentLevel line indentedLines
     | currentIndentation == indentLevel =
-        let (children, restIndentedChildLines) = parseIndent (currentIndentation + 1) (tail indentedLines);
-            (siblings, restIndentedSiblingLines) = parseIndent (currentIndentation + 1) (tail restIndentedChildLines);
-        in (Node (currentLineValue, 0, [], children) : siblings, restIndentedSiblingLines)
+        let (children, restIndentedChildLines) = parseIndent (currentIndentation + 1) (line + 1) (tail indentedLines)
+            (siblings, restIndentedSiblingLines) = parseIndent (currentIndentation + 1) (line + length indentedLines - length restIndentedChildLines) (tail restIndentedChildLines)
+        in (Node (currentLineValue, line, [], children) : siblings, restIndentedSiblingLines)
     | currentIndentation < indentLevel = ([] , indentedLines)
     | currentIndentation > indentLevel =
-        let (nodes, restIndentedLines) = parseIndent (currentIndentation + 1) (tail indentedLines);
-        in (Error ("Wrong indentation", (indentLevel, 0)) : nodes, restIndentedLines)
-
+        let (nodes, restIndentedLines) = parseIndent (currentIndentation + 1) (line + 1 )(tail indentedLines)
+        in (Error ("Wrong indentation", (line, indentLevel, indentLevel + length currentLineValue)) : nodes, restIndentedLines)
     where
-        (currentIndentation, currentLineValue) = head indentedLines;
+        (currentIndentation, currentLineValue) = head indentedLines
