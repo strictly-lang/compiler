010a004c04d184417642381300f68fb17e06b64e
plusgut <github@webentwickler2-0.de>
2021-03-21 16:47:46 +0100

    [ast] Implemented first draft of ast parsing

--- src-lib/Parser/Main.hs
+++ src-lib/Parser/Main.hs
@@ -1,13 +1,37 @@
 module Parser.Main where
 
-parse content = map parseLine (lines content)
+type LineCount = Int;
+type IndentedLine = (Int, String);
+type Option = [String];
+type Position = (Int, Int);
+type NodeName = String;
+type ErrorTuple = (String, Position);
+type NodeTuple = (NodeName, LineCount, [Option], [MaybeNode])
+data MaybeNode = Node NodeTuple | Error ErrorTuple;
 
+parse content = parseRoot( map getIndentation (lines content))
 
-parseLine x = let (indent, value ) = getIndentation x;
-            in show indent ++ ": " ++ value;
-
-getIndentation :: String -> (Int, [Char])
+getIndentation :: String -> IndentedLine
 getIndentation ('\t':xs) =
     let (indent, value) = getIndentation xs;
     in (indent + 1 , value)
-getIndentation otherwise = (0, otherwise);
+getIndentation xs = (0, xs);
+
+parseRoot :: [IndentedLine] -> [MaybeNode];
+parseRoot indentedLines = [Node ("foo", 1, [], [])]
+
+
+parseIndent :: LineCount -> [IndentedLine] -> ([MaybeNode], [IndentedLine])
+parseIndent _ [] = ([], [])
+parseIndent indentLevel indentedLines
+    | currentIndentation == indentLevel =
+        let (children, restIndentedChildLines) = parseIndent (currentIndentation + 1) (tail indentedLines);
+            (siblings, restIndentedSiblingLines) = parseIndent (currentIndentation + 1) (tail restIndentedChildLines);
+        in (Node (currentLineValue, 0, [], children) : siblings, restIndentedSiblingLines)
+    | currentIndentation < indentLevel = ([] , indentedLines)
+    | currentIndentation > indentLevel =
+        let (nodes, restIndentedLines) = parseIndent (currentIndentation + 1) (tail indentedLines);
+        in (Error ("Wrong indentation", (indentLevel, 0)) : nodes, restIndentedLines)
+
+    where
+        (currentIndentation, currentLineValue) = head indentedLines;
