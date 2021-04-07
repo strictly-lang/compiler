8979da8b1183715e58c3af3512e2bc68d32e9200
plusgut <github@webentwickler2-0.de>
2021-04-07 12:54:34 +0200

    [parser] improve line handling

--- src-lib/Parser/Main.hs
+++ src-lib/Parser/Main.hs
@@ -6,7 +6,7 @@ type Column = Int
 
 type IndentationLevel = Int
 
-type IndentedLine = (Int, String)
+type IndentedLine = (Line, IndentationLevel, String)
 
 type Option = [String]
 
@@ -14,47 +14,70 @@ type Position = (Line, Column)
 
 type NodeName = String
 
-type NodeTuple = (NodeName, Line, [Option], [Expr])
+newtype NodeTuple = NodeTuple (NodeName, Line, [Option], [Expr NodeTuple])
 
-data Expr = Node NodeTuple | TypeError String Position Position
+data Expr a = Node a | TypeError String Position Position
 
-type Parser a = [IndentedLine] -> ([a], [IndentedLine]);
+type Scanner a = [IndentedLine] -> ([Expr a], [IndentedLine])
 
-parse content = parseRoot (map getIndentation (lines content))
+parse content = parseRoot (getIndentedLines (lines content) 0)
 
-getIndentation :: String -> IndentedLine
+getIndentedLines :: [String] -> Line -> [IndentedLine]
+getIndentedLines [] _ = []
+getIndentedLines (l : ls) line
+  -- Empty line can be just be thrown away
+  | indentationLevel == 0 && value == "" = getIndentedLines ls (line + 1)
+  | otherwise = (indentationLevel, line, value) : getIndentedLines ls (line + 1)
+  where
+    (indentationLevel, value) = getIndentation l
+
+getIndentation :: String -> (IndentationLevel, String)
 getIndentation ('\t' : xs) =
   let (indent, value) = getIndentation xs
    in (indent + 1, value)
 getIndentation xs = (0, xs)
 
-parseRoot :: [IndentedLine] -> [Expr]
+parseRoot :: [IndentedLine] -> [Expr NodeTuple]
 parseRoot indentedLines =
-  let (rootNodes, restLines) = parseLines 0 0 indentedLines
+  let (rootNodes, restLines) = parseLines indentedLines 0
    in rootNodes
 
-parseLines :: IndentationLevel -> Line -> [IndentedLine] -> ([Expr], [IndentedLine])
-parseLines _ _ [] = ([], [])
-parseLines indentLevel line indentedLines
-  -- Empty Line
-  | currentLineValue == "" = parseLines indentLevel (line + 1) (tail indentedLines)
+parseLines :: [IndentedLine] -> IndentationLevel -> ([Expr NodeTuple], [IndentedLine])
+parseLines [] _ = ([], [])
+parseLines indentedLines indentLevel
   -- Current indent-level
   | currentIndentation == indentLevel =
     -- Children processing
-    let (children, restIndentedChildLines) = parseLines (currentIndentation + 1) (line + 1) (tail indentedLines)
+    let (children, restIndentedChildLines) = parseLines (tail indentedLines) (currentIndentation + 1)
         -- Sibling Processing
-        (siblings, restIndentedSiblingLines) = parseLines currentIndentation (line + length indentedLines - length restIndentedChildLines) (tail restIndentedChildLines)
-     in (Node (currentLineValue, line, [], children) : siblings, restIndentedSiblingLines)
+        (siblings, restIndentedSiblingLines) = parseLines (tail restIndentedChildLines) currentIndentation
+     in (Node (NodeTuple (currentLineValue, line, [], children)) : siblings, restIndentedSiblingLines)
   -- Outer indent level
   | currentIndentation < indentLevel = ([], indentedLines)
   -- To much indented
   | currentIndentation > indentLevel =
-    let (nodes, restIndentedLines) = parseLines currentIndentation (line + 1) (tail indentedLines)
-     in ( nodes ++ [TypeError
-            "Wrong indentation"
-            (line, indentLevel)
-            (line, indentLevel + length currentLineValue)],
+    let (nodes, restIndentedLines) = parseLines (tail indentedLines) currentIndentation
+     in ( nodes
+            ++ [ TypeError
+                   "Wrong indentation"
+                   (line, indentLevel)
+                   (line, indentLevel + length currentLineValue)
+               ],
           restIndentedLines
         )
   where
-    (currentIndentation, currentLineValue) = head indentedLines
+    (line, currentIndentation, currentLineValue) = head indentedLines
+
+-- parseWithScanner :: a => [Scanner a] -> IndentationLevel -> Line -> [IndentedLine] -> ([Expr a], [IndentedLine])
+-- -- No Scanners left
+-- parseWithScanner [] indentLevel line (l:ls) = [TypeError "Cant be parsed"
+--             (line, indentLevel)
+--             (line, indentLevel + length currentLineValue)]
+--   where
+--     (currentIndentation, currentLineValue) = head indentedLines
+-- parseWithScanner (scanner:scanners) indentLevel line indentedLines
+--   -- Scanner didnt take any lines, means scanner didnt care about lines
+--   | count scannedRestLines === count indentedLines = parseWithScanner scanners indentLevel line indentedLines
+--   | otherwise = (scannedExprResult, scannedRestLines)
+--   where
+--     (scannedExprResult, scannedRestLines) = scanner indentedLines
