57428e2ef64a1319055e8c174a9ec916eda52e0b
plusgut <github@webentwickler2-0.de>
2021-04-11 12:17:17 +0200

    fix(test): format

--- karma.conf.js
+++ karma.conf.js
@@ -27,8 +27,7 @@ module.exports = function (config) {
                                 resolve(stdout);
                             }
                         });
-                    }
-                    )
+                    })
                 }],
             },
             "karma-jasmine",
