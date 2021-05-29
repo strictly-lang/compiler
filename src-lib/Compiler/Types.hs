c1ae45a6df88f8a5728d2308448b99c3d12e6ec2
plusgut <github@webentwickler2-0.de>
2021-05-29 13:59:26 +0200

    chore(types): remove unneeded show-deriving

--- src-lib/Compiler/Types.hs
+++ src-lib/Compiler/Types.hs
@@ -9,16 +9,11 @@ type VariableStack = [([PublicVariableName], InternalVariableName)]
 type Scope = String
 
 newtype Context = Context (Scope, VariableStack)
-  deriving (Show)
 
 newtype UpdateCallbacks = UpdateCallbacks [(InternalVariableName, [Indent])]
-  deriving (Show)
 
 newtype RemoveCallbacks = RemoveCallbacks [Indent]
-  deriving (Show)
 
 newtype Predecessor = Predecessor String
-  deriving (Show)
 
 data Indent = Ln String | Br | Ind [Indent]
-  deriving (Show)
