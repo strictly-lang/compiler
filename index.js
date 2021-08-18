cefba05e7262009a3b4e37ede3524f381d98f586
Carlo Jeske <jeske@sc-networks.com>
2021-08-18 14:37:21 +0200

    fix(node): change node api to be more flexible

--- index.js
+++ index.js
@@ -1,28 +1,31 @@
+const os = require("os");
 const util = require("util");
 const exec = util.promisify(require("child_process").exec);
-const os = require("os");
 
-module.exports = async function ({ cwd, filePath }) {
-    let binary;
-    switch (os.platform()) {
-        case "darwin":
-            binary = "macos-x64/strictly";
-            break;
-        case "linux":
-            binary = "linux-x64/strictly";
-            break;
+module.exports = async function ({ cwd }) {
+    return function ({ filePath }) {
+        let binary;
+        switch (os.platform()) {
+            case "darwin":
+                binary = "macos-x64/strictly";
+                break;
+            case "linux":
+                binary = "linux-x64/strictly";
+                break;
 
-        default:
-            throw new Error("Your platform is not supported");
+            default:
+                throw new Error("Your platform is not supported");
+        }
+
+        const { stdout, stderror } = await exec(
+            `${__dirname}/dist/${binary} ${filePath}`, {
+            cwd
+        });
+
+        if (stderror) {
+            throw stderror;
+        }
+
+        return stdout;
     }
-    const { stdout, stderror } = await exec(
-        `${__dirname}/dist/${binary} ${filePath}`, {
-        cwd
-    });
-
-    if (stderror) {
-        throw stderror;
-    }
-
-    return stdout;
 }
