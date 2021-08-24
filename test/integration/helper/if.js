a4cf6734bb0d827f168ea53999612f9ee6591fe0
plusgut <github@webentwickler2-0.de>
2021-08-24 21:15:32 +0200

    fix(condition): fix updatehandling of if-helper during same condition #35

--- test/integration/helper/if.js
+++ test/integration/helper/if.js
@@ -53,7 +53,7 @@ describe("if condition handling", () => {
     expect(element.shadowRoot.childNodes[2].tagName).toBe("FOOTER");
   });
 
-  it("condition should render the correct result, false first", () => {
+  fit("condition should render the correct result, false first", () => {
     const element = document.createElement("test-components-helper-if-base");
     element.foo = false;
     element.bar = "baz";
@@ -94,5 +94,51 @@ describe("if condition handling", () => {
     expect(element.shadowRoot.childNodes[1].tagName).toBe("SPAN");
     expect(element.shadowRoot.childNodes[1].textContent).toBe("not-foo-baz");
     expect(element.shadowRoot.childNodes[2].tagName).toBe("FOOTER");
+  });
+
+  it("condition should render the correct update correct when both times condition is positive", () => {
+    const element = document.createElement("test-components-helper-if-base");
+    element.foo = true;
+    element.bar = "baz";
+    container.appendChild(element);
+
+    expect(element.shadowRoot.childNodes.length).toBe(3);
+    expect(element.shadowRoot.childNodes[0].tagName).toBe("HEADER");
+    expect(element.shadowRoot.childNodes[1].tagName).toBe("DIV");
+    expect(element.shadowRoot.childNodes[1].textContent).toBe("foo-baz");
+    expect(element.shadowRoot.childNodes[2].tagName).toBe("FOOTER");
+    let conditionContent = element.shadowRoot.childNodes[1];
+
+    element.foo = true;
+
+    expect(element.shadowRoot.childNodes.length).toBe(3);
+    expect(element.shadowRoot.childNodes[0].tagName).toBe("HEADER");
+    expect(element.shadowRoot.childNodes[1].tagName).toBe("DIV");
+    expect(element.shadowRoot.childNodes[1].textContent).toBe("foo-baz");
+    expect(element.shadowRoot.childNodes[2].tagName).toBe("FOOTER");
+    expect(element.shadowRoot.childNodes[1]).toBe(conditionContent);
+  });
+
+  it("condition should render the correct update correct when both times condition is negative", () => {
+    const element = document.createElement("test-components-helper-if-base");
+    element.foo = false;
+    element.bar = "baz";
+    container.appendChild(element);
+
+    expect(element.shadowRoot.childNodes.length).toBe(3);
+    expect(element.shadowRoot.childNodes[0].tagName).toBe("HEADER");
+    expect(element.shadowRoot.childNodes[1].tagName).toBe("SPAN");
+    expect(element.shadowRoot.childNodes[1].textContent).toBe("not-foo-baz");
+    expect(element.shadowRoot.childNodes[2].tagName).toBe("FOOTER");
+    let conditionContent = element.shadowRoot.childNodes[1];
+
+    element.foo = false;
+
+    expect(element.shadowRoot.childNodes.length).toBe(3);
+    expect(element.shadowRoot.childNodes[0].tagName).toBe("HEADER");
+    expect(element.shadowRoot.childNodes[1].tagName).toBe("SPAN");
+    expect(element.shadowRoot.childNodes[1].textContent).toBe("not-foo-baz");
+    expect(element.shadowRoot.childNodes[2].tagName).toBe("FOOTER");
+    expect(element.shadowRoot.childNodes[1]).toBe(conditionContent);
   });
 });
