2f36540829ae367d00135e9105847fd9ea5f7861
plusgut <github@webentwickler2-0.de>
2021-10-23 21:01:37 +0200

    feat(plugins): add js api for plugins

--- index.js
+++ index.js
@@ -2,30 +2,30 @@ const os = require("os");
 const util = require("util");
 const exec = util.promisify(require("child_process").exec);
 
-module.exports = function ({ cwd }) {
-    return async function ({ filePath }) {
-        let binary;
-        switch (os.platform()) {
-            case "darwin":
-                binary = "macos-x64/strictly";
-                break;
-            case "linux":
-                binary = "linux-x64/strictly";
-                break;
+module.exports = function ({ cwd, plugins }) {
+  return async function ({ filePath }) {
+    let binary;
+    switch (os.platform()) {
+      case "darwin":
+        binary = "macos-x64/strictly";
+        break;
+      case "linux":
+        binary = "linux-x64/strictly";
+        break;
 
-            default:
-                throw new Error("Your platform is not supported");
-        }
-
-        const { stdout, stderror } = await exec(
-            `${__dirname}/dist/${binary} ${filePath}`, {
-            cwd
-        });
-
-        if (stderror) {
-            throw stderror;
-        }
-
-        return stdout;
+      default:
+        throw new Error("Your platform is not supported");
     }
+
+    const { stdout, stderror } = await exec(
+      `${__dirname}/dist/${binary} ${plugins?.map(plugin => `--plugin=${plugin}`).join(" ") ?? ""} ${filePath}`, {
+      cwd
+    });
+
+    if (stderror) {
+      throw stderror;
+    }
+
+    return stdout;
+  }
 }
