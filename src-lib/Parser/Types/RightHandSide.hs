d03c5b1ae2c02e10f0016cace7fe6d0df09213d9
plusgut <github@webentwickler2-0.de>
2022-01-05 15:07:18 +0100

    feat(adt): add data declaration parsing

---
+++ src-lib/Parser/Types/RightHandSide.hs
@@ -0,0 +1,1 @@
+module Parser.Types.RightHandSide where
--- src-lib/Parser/Types/Root.hs
+++ src-lib/Parser/Types/Root.hs
@@ -2,12 +2,24 @@ module Parser.Types.Root where
 
 import Parser.Types
 import Parser.Util (assignParser, uppercaseIdentifierParser)
-import Text.Megaparsec.Char (space, space1, string)
+import Text.Megaparsec (between, many, optional, sepBy)
+import Text.Megaparsec.Char (char, space, space1, string)
 import Types
 
 dataParser :: Parser Root
 dataParser = do
-  name <- string "data" *> space1 *> uppercaseIdentifierParser <* space <* assignParser
-  return (RootDataDeclaration name [])
+  name <- string "data" *> space1 *> uppercaseIdentifierParser <* space
+  dataDeclarations <- between (assignParser <* space) (char ';' <* space) ((algebraicDataTypeParser <* space) `sepBy` (char '|' <* space))
+  return (RootDataDeclaration name dataDeclarations)
+
+algebraicDataTypeParser :: Parser DataDeclaration
+algebraicDataTypeParser = do
+  name <- uppercaseIdentifierParser
+  hasParameter <- optional (char '(')
+  parameters <-
+    case hasParameter of
+      Just _ -> do many algebraicDataTypeParser <* char ')'
+      Nothing -> do return []
+  return (DataDeclaration (name, parameters))
 
 rootParser = dataParser
