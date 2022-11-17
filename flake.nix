f6c5816634db0d970ede7004a894e624c7b6d9d5
plusgut <github@webentwickler2-0.de>
2022-11-17 09:29:26 +0100

    feat(nix): add dependency handling

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let pkgs = nixpkgs.legacyPackages.${system};
+        in {
+          devShells.default = pkgs.mkShell {
+              buildInputs = [ pkgs.nodejs pkgs.yarn pkgs.ghc pkgs.cabal-install pkgs.haskell-language-server];
+          };
+        }
+      );
+}
+
