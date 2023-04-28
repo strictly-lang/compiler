a5ded66cd5a28c78c3040270d51c3b1f800ee944
plusgut <github@webentwickler2-0.de>
2023-04-28 09:45:40 +0200

    chore(rust): init rewrite in rust

--- flake.nix
+++ flake.nix
@@ -2,23 +2,44 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    naersk.url = "github:nix-community/naersk";
+    rust-overlay.url = "github:oxalica/rust-overlay";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils, rust-overlay, naersk }:
     flake-utils.lib.eachDefaultSystem
       (system:
         let pkgs = nixpkgs.legacyPackages.${system};
             packageName = "strictly";
+            rustPkgs = import nixpkgs {
+              inherit system;
+              overlays = [ (import rust-overlay) ];
+            };
+            rustWithWasmTarget = rustPkgs.rust-bin.stable.latest.default.override {
+              targets = [ "wasm32-unknown-unknown" ];
+            };
+            naerskLib = pkgs.callPackage naersk {};
+
+            naerskLibWasm = pkgs.callPackage naersk {
+              rustc = rustWithWasmTarget;
+            };
+            app = naerskLibWasm.buildPackage {
+              name = "strictly";
+              src = ./.;
+              cargoBuildOptions = x: x ++ [ "-p" "strictly" ];
+              copyLibs = true;
+              CARGO_BUILD_TARGET = "wasm32-unknown-unknown";
+            };
         in {
-          packages.${packageName} = pkgs.haskellPackages.callCabal2nix packageName self rec {};
+          packages.${packageName} = app;
           defaultPackage = self.packages.${system}.${packageName};
 
           devShell = pkgs.mkShell {
             buildInputs = [
                 pkgs.nodejs
-                pkgs.ghc
-                pkgs.cabal-install
-                pkgs.haskell-language-server
+                pkgs.wasmer
+                pkgs.cargo
+                pkgs.rustfmt
               ];
           };
         }
--- src-lib/Prelude/Javascript/Types/Record.hs
+++
@@ -1,1 +0,0 @@
-module Prelude.Javascript.Types.Record where
